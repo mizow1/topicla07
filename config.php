d41fbaa0417b612d3787dee2853ee22dbdd139f6
mizow1 <mizow1@example.com>
2025-08-30 00:09:09 +0900

    APIキーを.envへ

--- config.php
+++ config.php
@@ -3,7 +3,10 @@ $servername = "localhost";
 $username = "xs483187_ow";
 $password = "password";
 $dbname = "xs483187_topicla07";
-$gemini = "AIzaSyCxe4eClwGhCeaZirNqQCMu3RHhrkXVy-E";
+$gemini = $_ENV['GEMINI_API_KEY'] ?? '';
+$openai = $_ENV['OPENAI_API_KEY'] ?? '';
+$claude = $_ENV['CLAUDE_API_KEY'] ?? '';
+
 
 try {
     $pdo = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password);
